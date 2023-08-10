843bdb8f197dd42579c060ad4c08aea649e2a1ae
KellyMark <kmark602@gmail.com>
2023-08-10 11:53:02 -0400

    commit tree 002f19e2cd3ecd91f828c4ffd8bf9eb988705534

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait_title 
+
+    def clickbait_title
+        if title
+            unless title.match?(/(Won't Believe)|(Secret)|(Guess)|(Top \d+)/)
+                errors.add(:title, "Please make the title more catchy")
+            end
+        end
+    end
 end
